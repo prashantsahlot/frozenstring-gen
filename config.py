612beb6bd0b8fc5245104062875578929c2b5991
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2025-07-07 21:38:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ API_ID = int(environ.get("API_ID", "20284828"))
 API_HASH = environ.get("API_HASH", "a980ba25306901d5c9b899414d6a9ab7")
 
 # Your Main Bot Token 
-BOT_TOKEN = environ.get("BOT_TOKEN", "7649485504:AAH3LSTdAOlZc1kjN3ZzpqDZBvvlPFLWA5E")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
-OWNER_ID = int(environ.get("OWNER_ID", "6797820880")) # Owner Id or Admin Id
+OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "-1002393689329")
+F_SUB = environ.get("F_SUB", "")
 
 # Mongodb Database Uri For User Data Store 
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://yacan69355:Cw92BrnfAfWQcLvU@cluster0.jh6h6wg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
 
 # Port To Run Web Application 
 PORT = int(environ.get('PORT', 8080))
